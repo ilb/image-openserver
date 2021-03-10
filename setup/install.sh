24e88ae2bf191bf424ecc3124e07950f559d076e
petrovk <petrovk86@mail.ru>
2021-03-11 01:11:11 +0400

    - error log php

--- setup/install.sh
+++ setup/install.sh
@@ -8,7 +8,6 @@ ln -vs ../config.php
 cd /etc/php7/cli
 ln -vs ../config.php
 
-mkdir /var/log/php
 mkdir /usr/share/php
 cd /usr/share/php 
 
@@ -16,3 +15,8 @@ git clone https://github.com/ilb/phplib.git .phplib-1 && ln -s .phplib-1/classes
 git clone https://github.com/ilb/happymeal.git .happymeal-1 && ln -s .happymeal-1/classes happymeal-1
 
 ln -s /usr/share/php/.happymeal-1/happymeal /usr/bin/happymeal-1
+
+mkdir /var/log/php
+cd /var/log/php
+echo "" > /var/log/php/php_error.log
+chmod -R 777 /var/log/php
